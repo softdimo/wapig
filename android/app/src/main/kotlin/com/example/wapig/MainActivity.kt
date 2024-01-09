057af2b4d17d8ce0130aa8f62565702c3d989c2a
Pablo Lara <pablo@Pablos-MacBook-Pro.local>
2024-01-09 11:42:39 -0500

    Se crea proyecto de wapig

---
+++ android/app/src/main/kotlin/com/example/wapig/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.wapig
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Material App',
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('WAPIG'),
+        ),
+        body: const Center(
+          child: Text('Welcome to wapig'),
+        ),
+      ),
+    );
+  }
+}
