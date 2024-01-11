3849756ee645a7ca77f3214adbe5114b75c4faa9
Pablo Lara <pablo06lara87@gmail.com>
2024-01-11 12:33:01 -0500

    Rama origen: master
    Rama actual de trabajo: req-20240109-01
    
    Archivos modificados:
    lib/presentation/screens/login.dart
    lib/presentation/screens/logos_imagenes.dart
    lib/presentation/screens/screen_welcome.dart
    
    Resumen del commit: Se crea la transición entre la pantalla de welcome y la pantalla de login. Se hace mediante una simulación de delayed. Se configuró el Provider.

---
+++ lib/presentation/screens/logos_imagenes.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:wapig/presentation/provider/discover_provider.dart';
+
+class LogoImagenes
+ extends StatelessWidget {
+  const LogoImagenes
+  ({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final discoverProvider = Provider.of<DiscoverProvider>(context);
+
+    return Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Center(
+            child: Image.asset(
+              'assets/images/logoWapig.png',
+              width: 300,
+              height: 300,
+            ),
+          ),
+          const Center(
+            child: Text(
+              'WAPIG',
+              style: TextStyle(
+                  fontFamily: 'Sansita',
+                  fontSize: 52,
+                  fontWeight: FontWeight.bold),
+            ),
+          ),
+          const SizedBox(height: 20,),
+
+          if (discoverProvider.initialLoading)
+            const Center(
+              child: CircularProgressIndicator(),
+            ),
+          Padding(
+            padding: const EdgeInsets.only(top: 50),
+            child: Center(
+              child: Image.asset(
+                'assets/images/logo_b_n.png',
+                width: 120,
+                height: 60,
+              ),
+            ),
+          ),
+        ],
+      );
+  }
+}
