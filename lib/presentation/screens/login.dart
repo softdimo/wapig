e51b1f1107196c32eab2f447a7a215c7c9612d8d
Pablo Lara <pablo06lara87@gmail.com>
2024-01-12 10:39:42 -0500

    Rama origen: master
    Rama actual de trabajo: req-20240112-03
    
    Archivos modificados:
    lib/presentation/screens/app_logo_images.dart
    lib/presentation/screens/login.dart
    lib/presentation/screens/logos_imagenes.dart
    lib/presentation/screens/screen_welcome.dart
    
    Resumen del commit:
    Se crea pantalla de login para prueba. Queda pendiente corregir algunos bugs

--- lib/presentation/screens/login.dart
+++ lib/presentation/screens/login.dart
@@ -1,16 +1,75 @@
+// ignore_for_file: library_private_types_in_public_api
+
 import 'package:flutter/material.dart';
 
-class Login extends StatelessWidget {
-  const Login({super.key});
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
 
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Login'),
+        title: const Text('Mi App'),
       ),
-      body: const Center(
-        child: Text('Esta es la pantalla de login'),
+      body: Container(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          children: [
+            // Logo
+            Image.asset(
+              'assets/images/logoWapig.png',
+              width: 300,
+              height: 300,
+            ),
+            // Texto
+            const Text('Mi App'),
+            // Botones
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                // Botón de registro
+                ElevatedButton(
+                  child: const Text('Registrarse'),
+                  onPressed: () {
+                    // Navegar a la pantalla de registro
+                  },
+                ),
+                // Botón de inicio de sesión
+                ElevatedButton(
+                  child: const Text('Iniciar sesión'),
+                  onPressed: () {
+                    // Iniciar sesión
+                  },
+                ),
+              ],
+            ),
+            // Inputs
+            const TextField(
+              decoration: InputDecoration(
+                labelText: 'Correo electrónico',
+              ),
+            ),
+            const TextField(
+              decoration: InputDecoration(
+                labelText: 'Contraseña',
+              ),
+            ),
+            // Botón de inicio de sesión
+            ElevatedButton(
+              child: const Text('Iniciar sesión'),
+              onPressed: () {
+                // Iniciar sesión
+              },
+            ),
+            // Texto de "olvidaste tu contraseña"
+            const Text('¿Olvidaste tu contraseña?'),
+          ],
+        ),
       ),
     );
   }
--- lib/presentation/screens/screen_welcome.dart
+++ lib/presentation/screens/screen_welcome.dart
@@ -1,25 +1,53 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:wapig/presentation/screens/login.dart';
-import 'package:wapig/presentation/screens/logos_imagenes.dart';
+import 'package:wapig/presentation/screens/app_logo_images.dart'; // Assuming renamed
 
-class ScreenWelcome extends StatelessWidget {
+class ScreenWelcome extends StatefulWidget {
   const ScreenWelcome({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  _ScreenWelcomeState createState() => _ScreenWelcomeState();
+}
 
-    //Se simula el llamado a la pantalla de login
-    Future.delayed(const Duration(seconds: 3), () {
-      // Navegar a la próxima pantalla (Home)
-      Navigator.pushReplacement(
+class _ScreenWelcomeState extends State<ScreenWelcome> {
+  late Timer? _timer;//Averiguar
+
+  @override
+  void initState() {
+    super.initState();
+    _timer = Timer(const Duration(seconds: 3), () {
+      _timer = null;
+      setState(() {
+        _isLoading = false;
+      });
+      Navigator.push(
         context,
-        MaterialPageRoute(builder: (context) => const Login()),
+        MaterialPageRoute(builder: (context) => const LoginScreen()),
       );
     });
-    
-    return const Scaffold(
-      backgroundColor: Color.fromARGB(255, 169, 242, 248),
-      body: LogoImagenes()
+  }
+
+  @override
+  void dispose() {
+    _timer?.cancel();
+    super.dispose();
+  }
+
+  bool _isLoading = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 169, 242, 248),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const AppLogoAndImages(),
+          _isLoading ? const CircularProgressIndicator() : Container(),
+        ],
+      ),
     );
   }
 }
