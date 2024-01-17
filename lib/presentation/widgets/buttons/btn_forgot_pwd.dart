3f455e74615ac6912dea16063bc1be43db129a1f
Juangui <jgmejiaco@gmail.com>
2024-01-17 15:14:24 -0500

    implementacion de boton olvide contraseña como componente

--- lib/presentation/widgets/buttons/btn_forgot_pwd.dart
+++ lib/presentation/widgets/buttons/btn_forgot_pwd.dart
@@ -11,10 +11,10 @@
 
 import 'package:flutter/material.dart';
 
-void main() => runApp(const MyApp());
+// void main() => runApp(const MyApp());
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class BtnForgotPwd extends StatelessWidget {
+  const BtnForgotPwd({super.key});
 
   @override
   Widget build(BuildContext context) {
