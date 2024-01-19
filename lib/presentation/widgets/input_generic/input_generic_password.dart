bf2039722794d7733dcd6e79f40a35962690ff57
Pablo Lara <pablo06lara87@gmail.com>
2024-01-19 11:10:11 -0500

    Se crea la clase input_generic_password con la lógica correspondiente para ocultar el texto en los inputs de tipo contraseña. Se inplementa la funcionalidad de cambio de pantalla en el texto de olvidaste tu contraseña mediante el envío de un parámetro en la llamada a la clase

---
+++ lib/presentation/widgets/input_generic/input_generic_password.dart
@@ -0,0 +1,104 @@
+import 'package:flutter/material.dart';
+
+class InputGenericPassword extends StatefulWidget {
+  final double width;
+  final double height;
+  final double borderRadius;
+  final Color borderColor;
+  final Color colorIcon;
+  final Color colorText;
+  final IconData iconType;
+  final String textHint;
+  final double fontSizeText;
+
+  const InputGenericPassword({
+    super.key,
+    this.width = 0.8,
+    this.height = 0.06,
+    this.borderRadius = 30,
+    this.borderColor = const Color.fromARGB(204, 173, 173, 178),
+    this.colorIcon = const Color.fromARGB(204, 173, 173, 178),
+    this.colorText = const Color.fromARGB(204, 173, 173, 178),
+    this.iconType = Icons.email,
+    required this.textHint,
+    required this.fontSizeText,
+  });
+
+  @override
+  State<InputGenericPassword> createState() => _InputGenericState();
+}
+
+class _InputGenericState extends State<InputGenericPassword> {
+  bool _obscureText = true;
+  bool _keyboardIsVisible = false;
+  final FocusNode _focusNode = FocusNode();
+
+  @override
+  void initState() {
+    super.initState();
+    _focusNode.addListener(() {
+      setState(() {
+        _keyboardIsVisible = _focusNode.hasFocus;
+      });
+    });
+  }
+
+  void _toggleObscureText() {
+    setState(() {
+      _obscureText = !_obscureText;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+
+    return NotificationListener<ScrollNotification>(
+      onNotification: (notification) {
+        if (notification is UserScrollNotification) {
+          if (_keyboardIsVisible) {
+            // Permitir el scroll
+            return false; // No hace nada, permite el scroll normal
+          } else {
+            // Impedir el scroll
+            return true; // Detiene el scroll
+          }
+        }
+        return false; // Permitir el scroll por defecto
+      },
+      child: SingleChildScrollView(
+        child: Container(
+          width: size.width * widget.width,
+          height: size.height * widget.height,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(widget.borderRadius),
+            border: Border.all(color: widget.borderColor),
+          ),
+          child: TextFormField(
+            focusNode: _focusNode,
+            obscureText: _keyboardIsVisible
+          ? _obscureText
+          : true,
+            decoration: InputDecoration(
+              border: InputBorder.none,
+              prefixIcon: Icon(
+                widget.iconType,
+                color: widget.colorIcon,
+              ),
+              suffixIcon: IconButton(icon: Icon(_obscureText
+                          ? Icons.visibility
+                          : Icons.visibility_off),
+                      onPressed: _toggleObscureText,
+                    ),
+              hintText: widget.textHint,
+              hintStyle: TextStyle(color: widget.colorText),
+            ),
+            style: TextStyle(
+              fontSize: widget.fontSizeText,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/presentation/widgets/title_text/title_name.dart
+++ lib/presentation/widgets/title_text/title_name.dart
@@ -7,6 +7,8 @@ class TitleName extends StatelessWidget {
   final double paddingRight;
   final double paddingTop;
   final double paddingBottom;
+  final VoidCallback? onPressed;
+  final bool isUnderLined;
 
   const TitleName({
     super.key, 
@@ -15,25 +17,31 @@ class TitleName extends StatelessWidget {
     this.paddingLeft = 0, 
     this.paddingRight = 0, 
     this.paddingTop = 30, 
-    this.paddingBottom = 30
+    this.paddingBottom = 30,
+    this.onPressed,
+    this.isUnderLined = false
   });
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.fromLTRB(
-        paddingLeft, 
-        paddingTop, 
-        paddingRight, 
-        paddingBottom
-      ),
-      child: Text(
-        welcomeText,
-        style: TextStyle(
-            fontFamily: 'Sansita', 
-            fontSize: fontSize, 
-            fontWeight: FontWeight.bold
-          ),
+    return GestureDetector(
+      onTap: onPressed,
+      child: Container(
+        padding: EdgeInsets.fromLTRB(
+          paddingLeft, 
+          paddingTop, 
+          paddingRight, 
+          paddingBottom
+        ),
+        child: Text(
+          welcomeText,
+          style: TextStyle(
+              fontFamily: 'Sansita', 
+              fontSize: fontSize, 
+              fontWeight: FontWeight.bold,
+              decoration: isUnderLined ? TextDecoration.underline : TextDecoration.none
+            ),
+        ),
       ),
     );
   }
